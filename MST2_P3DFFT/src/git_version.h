c11d8daddbdc7a16b63334e4664e80d4af0ff9e2
shoham sen <shohams@br006.pvt.bridges.psc.edu>
2020-03-07 15:05:10 -0500

    Contains code which does not use P3DFFT for FFTW

---
+++ MST2_P3DFFT/src/git_version.h
@@ -0,0 +1,1 @@
+write(myunit,'("# ",t5,a,t30,": ",a)') "Source code version","v2.5.2.9"
---
+++ MST2_P3DFFT/src/print_version_include.h
@@ -0,0 +1,7 @@
+! 
+! this routine is automatically generated at compilation time
+! DO NOT EDIT 
+! 
+   write(myunit,'("# ",t5,a,t30,": ",a)') "username",trim("shohams")
+   write(myunit,'("# ",t5,a,t30,": ",a)') "system name",trim("rigoletto.psc.edu")
+   write(myunit,'("# ",t5,a,t30,": ",a)') "code compiled",trim("Friday, December 13 2019 14:54:03")
